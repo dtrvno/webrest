576d58f29f8525f74f51cee7693a0fbecd0d4b10
dtrvno <dimatru@gmail.com>
2021-10-22 14:27:13 -0700

    added internationalization, swagger ui

--- src/main/java/com/dima/webrest/helloworld/HelloWorldController.java
+++ src/main/java/com/dima/webrest/helloworld/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.dima.webrest.helloworld;
 
 import com.dima.webrest.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
     //GET
     //URI- /hello-world
     //method - "hello
+    @Autowired
+    private MessageSource messageSource;
     @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello world";
@@ -21,5 +28,16 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathValirable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world ,%s",name));
+    }
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language",required = false)
+                                              Locale locale) {
+        return messageSource.getMessage("good.morning.message",
+                null,"Default Message",locale);
+    }
+    @GetMapping(path="/hello-world-internationalized1")
+    public String helloWorldInternationalized1() {
+        return messageSource.getMessage("good.morning.message",
+                null,"Default Message", LocaleContextHolder.getLocale());
     }
 }
