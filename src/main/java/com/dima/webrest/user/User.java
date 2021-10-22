040f3f6eaba436496640dac2bb90c2c06c180a1c
dtrvno <dimatru@gmail.com>
2021-10-22 12:28:21 -0700

    added validation

--- src/main/java/com/dima/webrest/user/User.java
+++ src/main/java/com/dima/webrest/user/User.java
@@ -1,5 +1,7 @@
 package com.dima.webrest.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
@@ -16,11 +18,11 @@ public class User {
     public Integer getId() {
         return id;
     }
-
+    @Size(min=2,message="Name should have at least 2 characters")
     public String getName() {
         return name;
     }
-
+    @Past
     public Date getBirthDate() {
         return birthDate;
     }
